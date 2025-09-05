502d040a71d50a6b88ddda1687c69e201f6d5df9
parkycai <parkycai@users.noreply.github.com>
2025-09-05 16:25:31 +0800

    Update oidc_discovery.go

--- object/oidc_discovery.go
+++ object/oidc_discovery.go
@@ -125,7 +125,7 @@ func GetOidcDiscovery(host string) OidcDiscovery {
 		IntrospectionEndpoint:                  fmt.Sprintf("%s/api/login/oauth/introspect", originBackend),
 		ResponseTypesSupported:                 []string{"code", "token", "id_token", "code token", "code id_token", "token id_token", "code token id_token", "none"},
 		ResponseModesSupported:                 []string{"query", "fragment", "form_post"},
-		GrantTypesSupported:                    []string{"password", "authorization_code"},
+		GrantTypesSupported:                    []string{"password", "authorization_code", "refresh_token"},
 		SubjectTypesSupported:                  []string{"public"},
 		IdTokenSigningAlgValuesSupported:       []string{"RS256", "RS512", "ES256", "ES384", "ES512"},
 		ScopesSupported:                        []string{"openid", "email", "profile", "address", "phone", "offline_access"},
